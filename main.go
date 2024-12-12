4b51cce01338ac1d0e978cdc4352a0b369ed37f1
mohits-git <152606488+mohits-git@users.noreply.github.com>
2024-12-12 23:47:56 +0530

    init: add tests for TDD and add clean input method

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"strings"
 )
+
+func cleanInput(text string) []string {
+  cleanedInput := []string{}
+  for _, word := range strings.Fields(text) {
+    cleanedInput = append(cleanedInput, strings.ToLower(word))
+  }
+  return cleanedInput
+}
 
 func main() {
     fmt.Println("Hello, World!")
